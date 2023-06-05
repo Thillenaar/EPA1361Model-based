f7afe9ddb88a9ca472de1e79992c3024799e899d
Thomas-4444 <125289385+Thomas-4444@users.noreply.github.com>
2023-06-05 23:23:27 +0200

    Update 3 optimalization

--- final assignment/dike_model_optimization.py
+++ final assignment/dike_model_optimization.py
@@ -7,9 +7,10 @@ from ema_workbench import (
     Scenario,
     SequentialEvaluator
 )
+from ema_workbench import Constraint
 from ema_workbench.em_framework.optimization import EpsilonProgress
 from ema_workbench.util import ema_logging
-
+import pandas as pd
 from problem_formulation import get_model_for_problem_formulation
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -46,7 +47,7 @@ if __name__ == "__main__":
     #print(len(model.outcomes))
     # for i in model.outcomes:
     #     print(i)
-    espilon = [100000000] * len(model.outcomes)
+    espilon = [10000000] * len(model.outcomes)
     #espilon = [1e3] * len(model.outcomes) #originele setting
 
     nfe = 10000  # 200 #proof of principle only, way to low for actual use
@@ -58,15 +59,41 @@ if __name__ == "__main__":
             epsilons=espilon,
             convergence=convergence_metrics,
             reference=ref_scenario,
+            #constraints=[Constraint("RfR Total Costs", outcome_names="RfR Total Costs",
+                        #  function=lambda x:max(0, x - 10000000))],
         )
 
+    pd.set_option("display.max_columns", None)
+    print(results)
     #Zelf toegevoegd op basis van code uitleg MOEA.
     from ema_workbench.analysis import parcoords
-    outcomes = results.loc[:, ['A.1 Expected Annual Damage', 'A.1_Expected Number of Deaths', 'A.2 Expected Annual Damage', 'A.2_Expected Number of Deaths', 'A.3 Expected Annual Damage', 'A.3_Expected Number of Deaths', 'A.4 Expected Annual Damage', 'A.4_Expected Number of Deaths', 'A.5 Expected Annual Damage', 'A.5_Expected Number of Deaths', 'RfR Total Costs', 'Expected Evacuation Costs', 'A.1 Dike Investment Costs', 'A.2 Dike Investment Costs', 'A.3 Dike Investment Costs', 'A.4 Dike Investment Costs', 'A.5 Dike Investment Costs']]
+    outcomes = results.loc[:, ['A.1 Expected Annual Damage', 'A.1_Expected Number of Deaths', 'A.2 Expected Annual Damage', 'A.2_Expected Number of Deaths', 'RfR Total Costs', 'Expected Evacuation Costs']]
+                               #'A.3 Expected Annual Damage', 'A.3_Expected Number of Deaths', 'A.4 Expected Annual Damage', 'A.4_Expected Number of Deaths', 'A.5 Expected Annual Damage', 'A.5_Expected Number of Deaths', 'A.1 Dike Investment Costs', 'A.2 Dike Investment Costs', 'A.3 Dike Investment Costs', 'A.4 Dike Investment Costs', 'A.5 Dike Investment Costs']]
+
+    # plot lines and determine legend
+    import matplotlib.patches as mpatches
+    legend_handles = []
+    import numpy as np
+    hsv = plt.get_cmap('hsv')
+    colors = hsv(np.linspace(0, 1.0, len(outcomes)))
+
     limits = parcoords.get_limits(outcomes)
     axes = parcoords.ParallelAxes(limits)
-    axes.plot(outcomes)
+
+    for i, (index, row) in enumerate(outcomes.iterrows()):
+        label = f"Policy {str(index)}"
+        clr = colors[i]
+
+        axes.plot(row.to_frame().T, color=clr, label=label)
+
+        patch = mpatches.Patch(color=clr, label=label)
+        legend_handles.append(patch)
+
+    plt.legend(handles=legend_handles)
     plt.show()
+
+    # export csv with results (levers and outcomes)
+    results.to_csv("OptimizationResults.csv")
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True)
     fig, ax1 = plt.subplots(ncols=1)
